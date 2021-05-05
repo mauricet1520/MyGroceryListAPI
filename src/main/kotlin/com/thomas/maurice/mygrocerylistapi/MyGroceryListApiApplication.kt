d77fb576e2ef27a1eb36474f3e445323236ff928
Maurice Thomas <mauricet1520@gmail.com>
2021-05-05 14:36:20 -0400

    Initial commit

---
+++ src/main/kotlin/com/thomas/maurice/mygrocerylistapi/MyGroceryListApiApplication.kt
@@ -0,0 +1,11 @@
+package com.thomas.maurice.mygrocerylistapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyGroceryListApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyGroceryListApiApplication>(*args)
+}
